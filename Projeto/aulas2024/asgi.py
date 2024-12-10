c4f3f0a04942979b5073f1776216cbaea631995d
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-12-10 11:27:29 -0300

    aula de hj

--- Projeto/aulas2024/asgi.py
+++ Projeto/aulas2024/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Atividade4 project.
+ASGI config for aulas2024 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Atividade4.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aulas2024.settings')
 
 application = get_asgi_application()
